c4a198cadf6e5d8ee8339c01a764cd316d170bf1
gurika666 <81112063+gurika666@users.noreply.github.com>
2023-11-03 16:40:49 +0400

    start

--- src/js/app.js
+++ src/js/app.js
@@ -11,15 +11,15 @@ import { AnimationUtils } from 'three';
 
 const canvas = document.querySelector('.canvas');
 
-let scene, camera, renderer, controls, composer;
+let scene, camera, renderer, controls, composer, mixer, animations;
 let mesh, envMap;
 let manager = new THREE.LoadingManager;
 let sceneloader = new GLTFLoader(manager);
 let hdriloader = new RGBELoader(manager);
+const clock = new THREE.Clock();
 
 manager.onLoad = function (){
   godswork();
-  animate();
 };
 hdriloader.load('images/hdri_05.hdr', function(hdri) {
   envMap = hdri;
@@ -29,57 +29,31 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
 });
 
 sceneloader.load('mesh/Cans.glb', function(gltf){
-  gltf.scene.traverse((child) => {
-    if (child.isMesh){
-
-      if(child.name != 'Retopo_Island' ){
-
-      const textureLoader = new THREE.TextureLoader()
-      const texture = textureLoader.load('images/aluminum_roughness.jpg')
-
-      const pbrMaterial = new THREE.MeshStandardMaterial();
-      pbrMaterial.copy(child.material);
-
-      
-      pbrMaterial.map = child.material.map;
-      pbrMaterial.roughness = 0.4;
-      pbrMaterial.roughnessMap = texture;
-      pbrMaterial.metalness = 0.7;
-      pbrMaterial.metalnessMap = child.material.metalnessMap;
-      
-      // pbrMaterial.envMap = envMap;
-
-      // Replace the original material with the PBR material
-      child.material = pbrMaterial;
-      child.castShadow = true;
-      child.receiveShadow = true;
-
-      }else{
-
-        child.receiveShadow = true;
-        
-      }
-    }
 
     mesh = gltf.scene;
+    camera = gltf.cameras[0];
 
-  })
+
+    animations = gltf.animations;
+    
+
 });
 
 function godswork() {
-    // Create the scene
+
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(36, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 1.5;
-    camera.position.y = 0.2;
-    // camera.position.x = -0.2;
-    // camera.rotation.x = -0.15;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+ 
    
     renderer = new THREE.WebGLRenderer({antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    THREE.ColorManagement.enabled = true;
-    renderer.outputColorSpace = THREE.SRGBColorSpace;
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    // THREE.ColorManagement.enabled = true;
+    // renderer.outputColorSpace = THREE.SRGBColorSpace;
+    // renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    document.body.appendChild(renderer.domElement);
+    window.addEventListener('resize', onWindowResize);
+
 
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.VSMShadowMap;
@@ -87,16 +61,19 @@ function godswork() {
   
     scene.environment = envMap;
     // scene.background = envMap;
-    scene.background = new THREE.Color(0x93CCC7);
+    // scene.background = new THREE.Color(0x93CCC7);
 
 
-    controls = new OrbitControls(camera, renderer.domElement);
+    // controls = new OrbitControls(camera, renderer.domElement);
     // controls.enabled = false;
 
+    mixer = new THREE.AnimationMixer(mesh);
 
-    document.body.appendChild(renderer.domElement);
+    for (let i = 0; i < animations.length; i++) {
+        const action = mixer.clipAction(animations[i]);
+        action.play();  
+    }
 
-    window.addEventListener('resize', onWindowResize);
     
 //PP
     const renderpass = new RenderPass(scene, camera);
@@ -105,7 +82,7 @@ function godswork() {
     composer = new EffectComposer(renderer, renderTarget);
     composer.setSize(window.innerWidth, window.innerHeight);
     composer.addPass(renderpass);
-    composer.addPass(new EffectPass(camera, new BrightnessContrastEffect({ brightness: -0.15, contrast: 0.3 })));
+    composer.addPass(new EffectPass(camera, new BrightnessContrastEffect({ brightness: -0.05, contrast: 0.01 })));
     composer.addPass(new EffectPass(camera, new SMAAEffect()));
     composer.addPass(new EffectPass(camera, new ChromaticAberrationEffect({offset: new THREE.Vector2(0.0002, 0.0002)})));
 
@@ -113,25 +90,33 @@ function godswork() {
 
 
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff,0.8);
-    directionalLight.position.set(1, 1, 0);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
+    // const directionalLight = new THREE.DirectionalLight(0xffffff,0.2);
+    // directionalLight.position.set(1, 1, 0);
+    // directionalLight.castShadow = true;
+    // scene.add(directionalLight);
 
-    directionalLight.shadow.mapSize.width = 1024; 
-    directionalLight.shadow.mapSize.height = 1024;
-    directionalLight.shadow.camera.near = 0.5; 
-    directionalLight.shadow.camera.far = 50;
-    directionalLight.shadow.radius = 10;
-    directionalLight.shadow.blurSamples = 25;
+    // directionalLight.shadow.mapSize.width = 1024; 
+    // directionalLight.shadow.mapSize.height = 1024;
+    // directionalLight.shadow.camera.near = 0.5; 
+    // directionalLight.shadow.camera.far = 50;
+    // directionalLight.shadow.radius = 100;
+    // directionalLight.shadow.blurSamples = 25;
 
+    // mesh.receiveShadow = true;
     scene.add(mesh);
+
+    animate();
 }
 
 function animate() {
+    const deltaTime = clock.getDelta();
+
+    mixer.update(deltaTime);
+
     requestAnimationFrame(animate);
     // renderer.render(scene, camera);
     composer.render();
+
 }
 
 function onWindowResize() {
