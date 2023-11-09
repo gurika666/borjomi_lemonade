7f4dcb23537c2ae3de3bf06288c37b30060735cb
gurika666 <81112063+gurika666@users.noreply.github.com>
2023-11-09 17:53:12 +0400

    almost finished

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,16 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
-import { EffectComposer, EffectPass, BrightnessContrastEffect, RenderPass, SMAAEffect, ChromaticAberrationEffect } from 'postprocessing';
+import { EffectComposer, EffectPass, BrightnessContrastEffect, RenderPass,ChromaticAberrationEffect,FXAAEffect, BloomEffect, DepthOfFieldEffect, BlendFunction } from 'postprocessing';
+import {VelocityDepthNormalPass, HBAOEffect, SSGIEffect} from 'realism-effects'
+
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const canvas = document.querySelector('.canvas');
 
 let scene, camera, renderer, composer, mixer , action,canmat, animations;
-let mesh, envMap, can;
+let mesh, envMap, can, rotation, dof;
 let manager = new THREE.LoadingManager;
 let sceneloader = new GLTFLoader(manager);
 let hdriloader = new RGBELoader(manager);
@@ -22,9 +24,14 @@ gsap.registerPlugin(ScrollTrigger);
 
 import vertexShader from "../shaders/vertexshader.glsl";
 import fragmentShader from "../shaders/fragmentShader.glsl";
+import { Vector3 } from 'three';
 let mandarinBase = textureLoader.load("/images/mandarin_basecolor.jpg");
 
-let textures = {}
+let textures = {};
+let fruits = [];
+
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
 
 const loadTextures = [
   {
@@ -80,6 +87,7 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
 
   sceneloader.load('mesh/Cans.glb', function(gltf){
     gltf.scene.traverse((child) => {
+      if(child.isMesh){
         if(child.name == "Can"){  
       
           canmat = new THREE.MeshPhysicalMaterial({
@@ -89,9 +97,10 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
             metalness: .6,
           })
           child.material = canmat
+          can = child;
           
         }
-        if(child.name == "Tarkhun"){  
+        else if(child.name == "Tarkhun"){  
       
           const material = new THREE.MeshPhysicalMaterial({
             map: textures.tarkhunbase,
@@ -102,7 +111,7 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
           child.material = material
           
         }
-        if(child.name == "Citrus"){  
+        else if(child.name == "Citrus"){  
       
           const material = new THREE.MeshPhysicalMaterial({
             map: textures.citrusbase,
@@ -113,7 +122,7 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
           child.material = material
           
         }
-        if(child.name == "Pear"){  
+        else if(child.name == "Pear"){  
       
           const material = new THREE.MeshPhysicalMaterial({
             map: textures.pearbase,
@@ -123,7 +132,10 @@ hdriloader.load('images/hdri_05.hdr', function(hdri) {
           })
           child.material = material
           
-        }
+        }else if (!child.name.includes("_metal") ){
+          fruits.push(child);
+        }}
+        // console.log(fruits)
     })
             
   mesh = gltf.scene;
@@ -146,34 +158,61 @@ function godswork() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   
-  renderer = new THREE.WebGLRenderer({antialias: true, alpha: true, canvas });
+  renderer = new THREE.WebGLRenderer({antialias: true, alpha: false, canvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
   window.addEventListener('resize', onWindowResize);
+  window.addEventListener('mousemove', onMouseMove);
       
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.VSMShadowMap;
 
-  scene.environment = envMap;
-  // scene.background = envMap;
-  
+  scene.fog = new THREE.Fog(0x3d0000, 2, 4.1);
 
+  scene.environment = envMap;
+  scene.background = new THREE.Color(0xffd000)
+  // scene.background = envMap;
+
+  // const velocityDepthNormalPass = new VelocityDepthNormalPass(scene, camera)
   
+   rotation = 0
+   
+
+   
     
-//PP
+  //PP
   const renderpass = new RenderPass(scene, camera);
   const parameters = {minFilter: THREE.LinearFilter,magFilter: THREE.LinearFilter,format: THREE.RGBAFormat,type: THREE.FloatType};
   const renderTarget = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, parameters );
   composer = new EffectComposer(renderer, renderTarget);
   composer.setSize(window.innerWidth, window.innerHeight);
   composer.addPass(renderpass);
-  composer.addPass(new EffectPass(camera, new BrightnessContrastEffect({ brightness: -0.05, contrast: 0.01 })));
-  composer.addPass(new EffectPass(camera, new SMAAEffect()));
-  composer.addPass(new EffectPass(camera, new ChromaticAberrationEffect({offset: new THREE.Vector2(0.0002, 0.0002)})));
+  
+  // composer.addPass(new EffectPass(camera, new BrightnessContrastEffect({ brightness: -0.05, contrast: 0.01 })));
+  
+  
+  dof = new DepthOfFieldEffect(camera, { resolutionScale: 1, worldFocusDistance: 2.5, worldFocusRange: 0.7, bokehScale: 3});
+  // composer.addPass(new EffectPass(camera, dof))
+  // console.log(dof.bokehScale)
+  
+  composer.addPass(new EffectPass(camera, new BloomEffect({intensity: 0.1,  radius: 0.6})));
 
-  // const light = new THREE.AmbientLight(10)
-  // scene.add(light)
+  // composer.addPass(new EffectPass(camera, new ChromaticAberrationEffect({offset: new THREE.Vector2(0.0001, 0.0001)})));
+  composer.addPass(new EffectPass(camera, new FXAAEffect()));
+
+
+ 
+
+
+  const light = new THREE.PointLight(0xffffff, 0.5)
+  light.position.y = 4
+  light.position.z = -1
+  // light.castShadow = true
+  scene.add(light)
+
   scene.add(mesh);
+
+ 
 
   createAnimation(mixer, action, animations[0]);
   
@@ -190,14 +229,58 @@ function animate() {
     requestAnimationFrame(animate);
     // renderer.render(scene, camera);
     composer.render(); 
-    const elapsedTime = clock.getElapsedTime();
+    // const elapsedTime = clock.getElapsedTime();
 
-    // canmat.uniforms.blendValue.value = (Math.sin(elapsedTime) + 1) / 2;
-    // canmat.uniforms.blendValue.value += 0.1;
-    // console.log(canmat.uniforms.blendValue.value)
+    fruits.forEach((fruit, index)=>{
+      fruit.rotation.z += 0.01
+     })
+
+     fruits.forEach((fruit) => {
+      // if (fruit.userData.velocity) {
+      //   fruit.position.add(fruit.userData.velocity);
+      // }
+      if (fruit.userData.angularVelocity) {
+        fruit.rotation.x += fruit.userData.angularVelocity.x;
+        fruit.rotation.y += fruit.userData.angularVelocity.y;
+        fruit.rotation.z += fruit.userData.angularVelocity.z;
+        dampenAngularVelocity(fruit, 0.98); // Adjust the damping factor as needed
+      }
+    
+    })
+
+    
 
 }
 
+function onMouseMove(event) {
+  // Calculate mouse coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Update the raycaster
+  raycaster.setFromCamera(mouse, camera);
+
+  // Check for intersections with the cube
+  const intersects = raycaster.intersectObjects(fruits);
+
+  if (intersects.length > 0) {
+    // Mouse is over the cube, trigger the impulse
+    const impulseForce = { x: 0.1, y: 0.1, z: 0.1}; // Adjust the force as needed
+    intersects.forEach((intersect) => {
+      applyImpulse(intersect.object, impulseForce);
+    });
+  }
+}
+function applyImpulse(object, force) {
+  // Simulate an impulse by updating the velocity or position
+  object.userData.angularVelocity = new THREE.Vector3(force.x, force.y, force.z);
+}
+
+function dampenAngularVelocity(object, dampingFactor) {
+  if (object.userData.angularVelocity) {
+    object.userData.angularVelocity.multiplyScalar(dampingFactor);
+  }
+}
 
 function createAnimation(mixer, action, clip) {
     let proxy = {
@@ -217,11 +300,14 @@ function createAnimation(mixer, action, clip) {
         start: "top top",
         endTrigger: ".container-end",
         scrub: true,
-        onUpdate: function () {
+        onUpdate: function (self) {
           camera.aspect = window.innerWidth / window.innerHeight;
           camera.updateProjectionMatrix();
           renderer.setSize(window.innerWidth, window.innerHeight);
+         
+        
         }
+       
       }
     });
 
@@ -243,21 +329,43 @@ function createAnimation(mixer, action, clip) {
 function changeShader(){
   ScrollTrigger.create({
     trigger: ".container2",
-    start: "top bottom",
-    end: "bottom bottom",
-    // markers: true,
-    onUpdate: (self) => {
-      // console.log(
-      //   "progress:",
-      //   self.progress.toFixed(3),
-      //   "direction:",
-      //   self.direction,
-      //   "velocity",
-      //   self.getVelocity()
-      // );
-      // canmat.uniforms.blendValue.value = self.progress.toFixed(3),
-      // console.log(canmat.uniforms.blendValue.value)
+    start: "center+=7% bottom",
+    end: "bottom+=20% bottom",
+    markers: true,
+    onLeaveBack:() =>{
+      canmat.map = textures.mandarinBase
+      // console.log("onelaveback")
     },
+    onEnter: (self) => {
+      canmat.map = textures.pearbase
+     
+      // console.log("enter")
+    },
+    onLeave:() => {
+      // console.log("onelave")
+      canmat.map = textures.citrusbase
+
+    },
+ 
+    onEnterBack:()=>{
+      canmat.map = textures.pearbase
+      // console.log("enterback")
+    }
+  });
+  ScrollTrigger.create({
+    trigger: ".container3",
+    start: "bottom bottom",
+    // end: "bottom+=35% bottom",
+    markers: true,
+    onLeaveBack:() =>{
+      canmat.map = textures.citrusbase
+      // console.log("onelaveback")
+    },
+    onEnter: (self) => {
+      canmat.map = textures.tarkhunbase
+      // console.log("enter")
+    },
+    
   });
 }
 
